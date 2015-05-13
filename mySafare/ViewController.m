5261c728bfb64690b8b61a3dc230b17a96f92b07
Tom Carmona <thomascarmona@gmail.com>
2015-05-13 14:46:14 -0500

    helloooo

--- mySafare/ViewController.m
+++ mySafare/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 
     NSString *string = @"hello";
+    NSString *secondString = @"elllooooo";
 }
 
 - (void)didReceiveMemoryWarning {
